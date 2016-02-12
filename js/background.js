3eb024c892f3b3d326a594a62ea3c4b2ab5e4831
Marko Kajzer <marko.kajzer@hotmail.de>
2016-02-12 05:37:06 +0100

    Fixed architecture
    Sending messages from background to content script was not the correct
    thing to do. Therefore, whenever the page action is clicked, execute
    the content script, send the result to background and update
    accordingly.

--- js/background.js
+++ js/background.js
@@ -1,7 +1,11 @@
+// Whenever clicked, execute content_script
 chrome.pageAction.onClicked.addListener(function(tab) {
-  console.log('triggered!');
-  chrome.tabs.sendMessage(tab.id, {"toggleLoopState" : true}, function(response) {
-    // Set icon according to new state
-    chrome.pageAction.setIcon({path: "icons/icon_" + response["state"] + ".png", tabId: tab.id});
+  chrome.tabs.executeScript(null, {
+    file: "js/content_script.js"
   });
+});
+
+// Receive message from content_script update accordingly
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  chrome.pageAction.setIcon({path: "icons/icon_" + request["state"] + ".png", tabId: sender.tab.id});
 });
--- js/content_script.js
+++ js/content_script.js
@@ -1,5 +1,5 @@
 // Change loop state and start video again if ended
-function toggleLoopState(sendResponse) {
+function toggleLoopState() {
   var video = document.querySelector('video');
 
   if(video.ended && !video.loop) {
@@ -9,11 +9,8 @@ function toggleLoopState(sendResponse) {
   video.loop = !video.loop;
 
   state = video.loop ? "on" : "off";
-  sendResponse({"state": state});
+  return { state: state };
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if(message['toggleLoopState'] != undefined && message['toggleLoopState'] == true) {
-    toggleLoopState(sendResponse);
-  }
-});
+// Send message to background.js
+chrome.runtime.sendMessage(toggleLoopState());
