e7da0604ca11381b27b051cf366539e1e9aab8e6
markokajzer <markokajzer91@gmail.com>
2016-11-09 16:29:14 +0900

    Added eslint and changed stuff accordingly

--- js/content_script.js
+++ js/content_script.js
@@ -1,19 +1,17 @@
-'use strict';
-
 const TITLE_OFF = 'Loop Video';
 const TITLE_ON = 'Stop Looping';
 const COLOR_OFF = '#fff';
 const COLOR_ON = '#f12b24';
 const DEGREE_OFF = 0;
 const DEGREE_ON = 90;
-var rotated = false;
+let rotated = false;
 
 // Function to create svg for button
 function getSVG() {
-  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute('viewBox', '0 0 24 24');
   svg.setAttribute('fill', COLOR_OFF);
-  var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   path.setAttribute('d', 'M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z');
   svg.appendChild(path);
   return svg;
@@ -21,12 +19,12 @@ function getSVG() {
 
 // Add a button for toggling loop to the page
 function addToggleControls() {
-  var controls = document.querySelector('div.ytp-left-controls');
-  if(!controls) return false;
-  var newButton = document.createElement('a');
+  const controls = document.querySelector('div.ytp-left-controls');
+  if (!controls) return false;
+  const newButton = document.createElement('a');
   newButton.className = 'ytp-button youloop-button';
   newButton.title = TITLE_OFF;
-  newButton.addEventListener('click', function() { toggleLoopState(); });
+  newButton.addEventListener('click', () => toggleLoopState());
   newButton.appendChild(getSVG());
   controls.appendChild(newButton);
   return true;
@@ -34,68 +32,64 @@ function addToggleControls() {
 
 // Change loop state and start video again if ended
 function toggleLoopState() {
-  var video = document.querySelector('video');
+  const video = document.querySelector('video');
 
-  if(video.ended && !video.loop) {
+  if (video.ended && !video.loop)
     video.play();
-  }
   video.loop = !video.loop;
   rotated = !rotated;
 
-  var color = video.loop ? COLOR_ON : COLOR_OFF;
-  var title = video.loop ? TITLE_ON : TITLE_OFF;
-  var degree = video.loop ? DEGREE_ON : DEGREE_OFF;
+  const color = video.loop ? COLOR_ON : COLOR_OFF;
+  const title = video.loop ? TITLE_ON : TITLE_OFF;
+  const degree = video.loop ? DEGREE_ON : DEGREE_OFF;
   updateToggleControls(color, title, degree);
 }
 
 // Update title and color of loop controls
 function updateToggleControls(newColor, newTitle, newDegree) {
-  var svg = document.querySelector('.youloop-button svg');
+  const svg = document.querySelector('.youloop-button svg');
   svg.style.fill = newColor;
-  svg.style.transform = 'rotate(' + newDegree + 'deg)';
+  svg.style.transform = `rotate(${newDegree}deg)`;
   document.querySelector('.youloop-button').setAttribute('title', newTitle);
-  let checkbox = document.querySelector('[role=menuitemcheckbox]');
-  if(checkbox !== null) {
+  const checkbox = document.querySelector('[role=menuitemcheckbox]');
+  if (checkbox !== null)
     checkbox.setAttribute('aria-checked', rotated);
-  }
 }
 
 // Add observer to video element to check if source was changed
 // This happens when changing to another video in a playlist
 function addObserver() {
-  var video = document.querySelector('video');
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      if(mutation.attributeName === 'src'  ||
+  const video = document.querySelector('video');
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.attributeName === 'src' ||
         (mutation.attributeName === 'loop' && video.getAttribute('loop') === null && rotated === true)) {
         rotated = false;
         updateToggleControls(COLOR_OFF, TITLE_OFF, DEGREE_OFF);
         document.querySelector('[role=menuitemcheckbox]').setAttribute('aria-checked', rotated);
-      }
-      else if(mutation.attributeName === 'loop' && video.getAttribute('loop') !== null  && rotated === false) {
+      } else if (mutation.attributeName === 'loop' && video.getAttribute('loop') !== null && rotated === false) {
         rotated = true;
         updateToggleControls(COLOR_ON, TITLE_ON, DEGREE_ON);
         document.querySelector('[role=menuitemcheckbox]').setAttribute('aria-checked', rotated);
       }
     });
   });
-  var config = { attributes: true };
-  observer.observe(video, config);
+  observer.observe(video, { attributes: true });
 }
 
 function addContextMenuListener() {
-  let video = document.querySelector('video');
-  video.addEventListener('contextmenu', function() {
+  const video = document.querySelector('video');
+  video.addEventListener('contextmenu', () => {
     let checkbox = null;
-    let interval = setInterval(function() {
+    const interval = setInterval(() => {
       checkbox = document.querySelector('[role=menuitemcheckbox]');
-      if(checkbox !== null) {
-        let oldElement = checkbox;
-        let newElement = oldElement.cloneNode(true);
+      if (checkbox !== null) {
+        const oldElement = checkbox;
+        const newElement = oldElement.cloneNode(true);
         newElement.setAttribute('aria-checked', rotated);
-        newElement.addEventListener('click', function() {
+        newElement.addEventListener('click', () => {
           toggleLoopState();
-          setTimeout(function() {
+          setTimeout(() => {
             document.querySelector('.ytp-contextmenu').style.display = 'none';
           }, 100);
         });
@@ -108,8 +102,8 @@ function addContextMenuListener() {
 
 // Init this whole thing!
 function init() {
-  var checkForVideo = setInterval(function () {
-    if(addToggleControls()) {
+  const checkForVideo = setInterval(() => {
+    if (addToggleControls()) {
       addObserver();
       addContextMenuListener();
       clearInterval(checkForVideo);
