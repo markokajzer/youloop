7b8b3a741d0397f0a549a4c84bf54f404a42ebcb
markokajzer <markokajzer91@gmail.com>
2018-05-06 02:05:35 +0200

    Improved width for fullscreen
    Reworked YouLoop
    Housekeeping

--- js/content_script.js
+++ js/content_script.js
@@ -1,116 +1,81 @@
-const TITLE_OFF = 'Loop Video';
-const TITLE_ON = 'Stop Looping';
-const COLOR_OFF = '#fff';
-const COLOR_ON = '#f12b24';
-const DEGREE_OFF = 0;
-const DEGREE_ON = 90;
-let rotated = false;
-
-// Function to create svg for button
-function getSVG() {
+function getYouLoopSVG() {
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute('viewBox', '0 0 24 24');
-  svg.setAttribute('fill', COLOR_OFF);
   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   path.setAttribute('d', 'M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z');
   svg.appendChild(path);
   return svg;
 }
 
-// Add a button for toggling loop to the page
-function addToggleControls() {
-  const controls = document.querySelector('div.ytp-left-controls');
-  if (!controls) return false;
-  const newButton = document.createElement('a');
-  newButton.className = 'ytp-button youloop-button';
-  newButton.title = TITLE_OFF;
-  newButton.addEventListener('click', () => toggleLoopState());
-  newButton.appendChild(getSVG());
-  controls.appendChild(newButton);
-  return true;
+
+// Init this whole thing!
+function init() {
+  setTimeout(videoElementPresent() ? addYouLoop : init, 500);
 }
 
-// Change loop state and start video again if ended
+function videoElementPresent() {
+  return document.querySelector('video') !== null;
+}
+
+function addYouLoop() {
+  insertYouLoopElement();
+  addObserver();
+  addContextMenuListener();
+}
+
+function insertYouLoopElement() {
+  const newButton = document.createElement('a');
+  newButton.classList.add('ytp-button', 'youloop-button');
+  newButton.title = 'Loop Video';
+  newButton.appendChild(getYouLoopSVG());
+  newButton.addEventListener('click', toggleLoopState);
+
+  document.querySelector('div.ytp-left-controls').appendChild(newButton);
+}
+
 function toggleLoopState() {
   const video = document.querySelector('video');
-
-  if (video.ended && !video.loop)
-    video.play();
   video.loop = !video.loop;
-  rotated = !rotated;
+  if (video.ended && video.loop) video.play();
 
-  const color = video.loop ? COLOR_ON : COLOR_OFF;
-  const title = video.loop ? TITLE_ON : TITLE_OFF;
-  const degree = video.loop ? DEGREE_ON : DEGREE_OFF;
-  updateToggleControls(color, title, degree);
+  updateToggleControls();
 }
 
-// Update title and color of loop controls
-function updateToggleControls(newColor, newTitle, newDegree) {
-  const svg = document.querySelector('.youloop-button svg');
-  svg.style.fill = newColor;
-  svg.style.transform = `rotate(${newDegree}deg)`;
-  document.querySelector('.youloop-button').setAttribute('title', newTitle);
-  const checkbox = document.querySelector('[role=menuitemcheckbox]');
-  if (checkbox !== null)
-    checkbox.setAttribute('aria-checked', rotated);
+function updateToggleControls() {
+  const youloop = document.querySelector('.youloop-button');
+  youloop.classList.toggle('active');
+  youloop.setAttribute('title', isActive() ? 'Stop Looping' : 'Loop Video');
 }
 
-// Add observer to video element to check if source was changed
-// This happens when changing to another video in a playlist
+function isActive() {
+  const youloop = document.querySelector('.youloop-button');
+  return youloop.classList.contains('active');
+}
+
+// Adjust toggle controls when loop attribute of video is
+// changed externally, e.g. via native context menu
 function addObserver() {
   const video = document.querySelector('video');
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.attributeName === 'loop') {
-        if (video.getAttribute('loop') === null && rotated) {
-          rotated = false;
-          updateToggleControls(COLOR_OFF, TITLE_OFF, DEGREE_OFF);
-        }
-        else if (video.getAttribute('loop') !== null && !rotated) {
-          rotated = true;
-          updateToggleControls(COLOR_ON, TITLE_ON, DEGREE_ON);
-        }
-      }
+  new MutationObserver((mutations) => {
+    mutations.forEach(() => {
+      if ((video.getAttribute('loop') === null && isActive()) ||
+          (video.getAttribute('loop') !== null && !isActive())) updateToggleControls();
     });
-  });
-  observer.observe(video, { attributes: true });
+  }).observe(video, { attributes: true, attributeFilter: ['loop'] });
 }
 
-// Add control over the YouTube context menu
+// Change state of YouTube context menu according to YouLoop when opening
 function addContextMenuListener() {
   const video = document.querySelector('video');
   video.addEventListener('contextmenu', () => {
-    let checkbox = null;
-    const interval = setInterval(() => {
-      checkbox = document.querySelector('[role=menuitemcheckbox]');
-      if (checkbox !== null) {
-        const oldElement = checkbox;
-        const newElement = oldElement.cloneNode(true);
-        newElement.setAttribute('aria-checked', rotated);
-        newElement.addEventListener('click', () => {
-          toggleLoopState();
-          setTimeout(() => {
-            document.querySelector('.ytp-contextmenu').style.display = 'none';
-          }, 100);
-        });
-        oldElement.parentNode.replaceChild(newElement, oldElement);
-        clearInterval(interval);
-      }
+    setTimeout(() => {
+      const checkbox = document.querySelector('[role=menuitemcheckbox]');
+      checkbox.setAttribute('aria-checked', isActive());
+      checkbox.addEventListener('click', toggleLoopState);
     }, 50);
   });
 }
 
-// Init this whole thing!
-function init() {
-  const checkForVideo = setInterval(() => {
-    if (addToggleControls()) {
-      addObserver();
-      addContextMenuListener();
-      clearInterval(checkForVideo);
-    }
-  }, 500);
-}
 
 init();
 console.log('Enjoy the awesome music!');
